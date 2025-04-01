9f5ad18dccba13917936f39cb35892213de49d64
prince-ravi-leow <princeravileow@outlook.com>
2025-04-01 23:56:32 +0200

    massive refactoring

--- tracksplit.py
+++ tracksplit.py
@@ -1,161 +1,193 @@
-#!/usr/bin/env python3
-
-# Wishlist:
-# * Option to process video files 
+"""Python-based tool for audio file track splitting"""
 
 import os
+import textwrap
+from argparse import ArgumentParser, RawDescriptionHelpFormatter
 from datetime import datetime
 
+from ffcuesplitter.user_service import FileSystemOperations
+
+
+def create_args_parser():
+    """Create argument parser"""
+
+    parser = ArgumentParser(
+        formatter_class=RawDescriptionHelpFormatter,
+        description=textwrap.dedent(
+            """   
+        The purpose of this program is: given an existing FFmpeg installation, to split a source audio file using provided 'YouTube style' timestamps e.g.: 
+        
+        00:55:45 Pink Elephants on Parade
+        01:20:54 Circle of Life
+        02:54:01 Colours of the Wind
+        """
+        ),
+    )
+
+    parser.add_argument(
+        "timestamps",
+        type=str,
+        help="File containing timestamps",
+    )
+    parser.add_argument("audio", type=str, help="Audio file source")
+    parser.add_argument("--artist", dest="artist", type=str, help="Artist name")
+    parser.add_argument("--album", dest="album", type=str, help="Album name")
+    parser.add_argument(
+        "--only-cue",
+        action="store_true",
+        default=False,
+        help="Optional: Output cue file without performing split, when supplied with 'True' argument (Default: False)",
+    )
+
+    return parser.parse_args()
+
+
 def datetime_transform(timecode):
-	"""
-	Detect whether a timecode is %M:%S or %H:%M:%S' (uses the revolutionary method of COUNTING number of colons - AMAZING I know!), 
-	uses datetime module and hms2msf helping function to return a cuesheet compatible timecode string
-	"""
+    """
+    Detect whether a timecode is %M:%S or %H:%M:%S'
+    """
 
-	def hms2msf(timecode_dt):
-		"""
-		Given a datetime object with the %H:%M:%S format, convert to cuesheet compatible %M:%S:%f timestamp, return respective string 
-		Note: this iteration populates the %f component with a single double-zero padding (:00)
-		"""
-		minutes = timecode_dt.hour*60 + timecode_dt.minute
-		seconds = timecode_dt.second
-		msf = f"{minutes}:{seconds:02d}:00"
-		return msf
+    def hms2msf(timecode_dt):
+        """
+        Given a datetime object with the %H:%M:%S format, convert to cuesheet compatible %M:%S:%f timestamp, return respective string
+        Note: this iteration populates the %f component with a single double-zero padding (:00)
+        """
+        minutes = timecode_dt.hour * 60 + timecode_dt.minute
+        seconds = timecode_dt.second
+        msf = f"{minutes}:{seconds:02d}:00"
+        return msf
 
-	if timecode.count(":") == 1:
-		timecode_dt = datetime.strptime(timecode, '%M:%S').time()
-		timecode_string = timecode_dt.strftime('%M:%S:00')
-	
-	if timecode.count(":") == 2:
-		timecode_dt = datetime.strptime(timecode, '%H:%M:%S').time()
-		timecode_string = hms2msf(timecode_dt)
-	
-	return timecode_string
+    assert ":" in timecode, "Timecode could not be converted"
 
-def extract_elements(timestamps_file):
-	"""
-	Given a text file containing 'YouTube-style timestamps' (where timecodes and titles are separated by a single white-space), separate timecodes and track strings into their constituent lists. 
-	This iteration also converts the time strings into the cue-sheet compatible %M:%S:%f timestamp format.
-	"""
-	timecode_cue = []
-	track_string = []
-	
-	with open(timestamps_file, "rt") as timestamps:
-		for line in timestamps:
-			timecode = line.split(" ")[0]
-			timecode_string = datetime_transform(timecode)
-			timecode_cue.append(timecode_string)
-			track_string.append(" ".join(line.split(" ")[1:]))	
-	
-	return timecode_cue, track_string 
+    if timecode.count(":") == 1:
+        timecode_dt = datetime.strptime(timecode, "%M:%S").time()
+        timecode_string = timecode_dt.strftime("%M:%S:00")
 
-def make_cue(timecode, track_string, audio_file, artist, album):
-	"""
-	Generates a cue sheet for track splitting, given: 
-	* timecode and title strings (of eaqual length)
-	* source audio filename
-	* respective artist (PERFORMER) and album (TITLE)
-	"""
-	# Determine file type
-	file_ext = audio_file.split(".")[-1]
-	if file_ext == 'wav':
-		file_type = 'wave'
-	else:
-		file_type = file_ext
-	
-	# Build cue sheet from timecodes and tracks
-	cue_sheet = f'PERFORMER "{artist}"\nTITLE "{album}"\nFILE "{audio_file}" {file_type.upper()}\n'
+    if timecode.count(":") == 2:
+        timecode_dt = datetime.strptime(timecode, "%H:%M:%S").time()
+        timecode_string = hms2msf(timecode_dt)
 
-	for i, (track_string, timecode) in enumerate(zip(track_string, timecode)):
-		track_number = i + 1
-		cue_sheet += f'\tTRACK {track_number:02d} AUDIO\n'
-		cue_sheet += f'\t\tTITLE "{track_string}"\n'
-		cue_sheet += f'\t\tINDEX 01 {timecode}\n'
-	
-	# Write cue sheet
-	audiofile_basename = os.path.basename(audio_file)	
-	cuesheet_filename = f"{os.path.splitext(audiofile_basename)[0]}.cue"
-	
-	with open(cuesheet_filename, "wt") as f:
-		f.write(cue_sheet)
-	
-	return cuesheet_filename
+    return timecode_string
 
-def tracksplit(timestamps_file, audio_file, artist, album, only_cue):
-	"""
-	Split source audio file into individual tracks, given a timestamps file using FFmpeg (option to generate cue sheet without performing split)
-	"""
-	timecode, track_string = extract_elements(timestamps_file)
 
-	cuesheet_filename = make_cue(timecode, track_string, audio_file, artist, album)
-		
-	if not only_cue:
-		try:
-			from ffcuesplitter.user_service import FileSystemOperations
-		except ImportError:
-			print("The required module 'ffcuesplitter' is not found.\nPlease install it by running 'python3 -m pip install ffcuesplitter'")
-			exit(1)
-		
-		# Make results directory
-		out_dir = os.path.join(os.getcwd(), f'{artist}_{album}_tracksplit')
-		if not os.path.exists(out_dir):
-			os.mkdir(out_dir)
-		
-		### ffcuesplitter block ###
-		file_type = audio_file.split(".")[-1]    
-		split = FileSystemOperations(
-			filename = cuesheet_filename,
-			outputdir = out_dir, 
-			outputformat = f"{file_type}",
-			ffmpeg_add_params = '-map 0:a', # to avoid errors with cover art 
-			dry = False, 
-			prg_loglevel = 'info')
-		if split.kwargs['dry']:
-			split.dry_run_mode()
-		else:
-			overwr = split.check_for_overwriting()
-			if not overwr:
-				split.work_on_temporary_directory()
-				
-if __name__ == '__main__':
-	from argparse import ArgumentParser, RawDescriptionHelpFormatter
-	import textwrap
+def extract_timestamps(timestamps_file):
+    """
+    Given a text file containing 'YouTube-style timestamps' (where timecodes and titles are separated by a single white-space), separate timecodes and track strings into their constituent lists.
+    This iteration also converts the time strings into the cue-sheet compatible %M:%S:%f timestamp format.
+    """
+    timecode_cue = []
+    track_string = []
 
-	parser = ArgumentParser(
-		formatter_class = RawDescriptionHelpFormatter,
-		description = textwrap.dedent("""
-		
-		*** EXAMPLE USAGE: *** 
+    with open(timestamps_file, "rt", encoding="utf-8") as timestamps:
+        for line in timestamps:
+            timecode = line.split(" ")[0]
+            timecode_string = datetime_transform(timecode)
+            timecode_cue.append(timecode_string)
+            track_string.append(" ".join(line.split(" ")[1:]))
 
-		python3 tracksplit.py --timestamps 'timestamps.txt' --audio 'concert_audio.mp3' --artist 'Sensible Clown Conglomerate' --album 'The Light Side of the Sun'
-		
-		The purpose of this program is: given an existing FFmpeg installation, to split a source audio file using provided 'YouTube style' timestamps e.g.: 
-		
-		00:55:45 Pink Elephants on Parade
-		01:20:54 Circle of Life
-		02:54:01 Colours of the Wind
+    return timecode_cue, track_string
 
-		Running the entire track-splitting pipeline requires a single Python dependency (FFcuesplitter).
 
-		HOWEVER, should one desire to perform the actual track split with a software of their choice, one can obtain a source audio file-specific cue sheet by providing setting the --only-cue to True, which will BYPASS the dependency entirely.
-		"""))
+def make_cue(timecode, track_string, audio_file, artist, album, out_dir):
+    """
+    Generates a cue sheet for track splitting, given:
+    * timecode and title strings (of eaqual length)
+    * source audio filename
+    * respective artist (PERFORMER) and album (TITLE)
+    """
+    # Determine audio codec
+    ext_to_codec = {
+        "flac": "flac",
+        "mp3": "mp3",
+        "ogg": "ogg",
+        "opus": "opus",
+        "ape": "ape",
+        "wav": "wave",
+        "m4a": "aac",
+    }
+    audio_codec = ext_to_codec.get(audio_file.split(".")[-1], None)
+    assert audio_codec, "Audio codec couldn't be determined"
 
-	parser.add_argument("--timestamps", dest="timestamps_file", type=str, help="File containing timestamps")
-	parser.add_argument("--audio", dest="audio_file", type=str, help="Source audio file")
-	parser.add_argument("--artist", dest="artist", type=str, help="Artist name")
-	parser.add_argument("--album", dest="album", type=str, help="Album name")
-	parser.add_argument("--only-cue", action="store_true", dest="only_cue", default=False, help="Optional: Output cue file without performing split, when supplied with 'True' argument (Default: False)")
+    # Build cue sheet from timecodes and tracks
+    cue_sheet = f'PERFORMER "{artist}"\nTITLE "{album}"\nFILE "{audio_file}" {audio_codec.upper()}\n'
 
-	args = parser.parse_args()
-	timestamps_file = args.timestamps_file
-	audio_file = args.audio_file
-	artist = args.artist
-	album = args.album
-	only_cue = args.only_cue
+    for i, (track_string, timecode) in enumerate(zip(track_string, timecode)):
+        track_number = i + 1
+        cue_sheet += f"\tTRACK {track_number:02d} AUDIO\n"
+        cue_sheet += f'\t\tTITLE "{track_string}"\n'
+        cue_sheet += f"\t\tINDEX 01 {timecode}\n"
 
-	tracksplit(
-		timestamps_file, 
-		audio_file, 
-		artist, 
-		album, 
-		only_cue)
+    # Write cue sheet
+    audiofile_basename = os.path.basename(audio_file)
+    cuesheet_filename = os.path.join(
+        out_dir, f"{os.path.splitext(audiofile_basename)[0]}.cue"
+    )
+
+    with open(cuesheet_filename, "wt", encoding="utf-8") as f:
+        f.write(cue_sheet)
+
+    return cuesheet_filename
+
+
+def get_output_fmt(audio_file):
+    """Determine ffcuesplitter output format"""
+    valid_formats = ("flac", "mp3", "ogg", "opus")
+    file_ext = audio_file.split(".")[-1]
+    for fmt in valid_formats:
+        if fmt in valid_formats:
+            output_fmt = fmt
+            return output_fmt
+    if file_ext == "wav":
+        output_fmt = "wave"
+    else:
+        output_fmt = "copy"
+    return output_fmt
+
+
+def call_ffcuesplitter(audio_file, cuesheet_filename, out_dir):
+    """
+    Split source audio file into individual tracks, given a timestamps file using FFmpeg (option to generate cue sheet without performing split)
+    """
+    output_format = get_output_fmt(audio_file)
+    split = FileSystemOperations(
+        filename=cuesheet_filename,
+        outputdir=out_dir,
+        outputformat=f"{output_format}",
+        ffmpeg_add_params="-map 0:a",  # to avoid errors with cover art
+        dry=False,
+        prg_loglevel="info",
+    )
+    if split.kwargs["dry"]:
+        split.dry_run_mode()
+    else:
+        overwr = split.check_for_overwriting()
+        if not overwr:
+            split.work_on_temporary_directory()
+
+
+def main():
+    """Main function"""
+    args = create_args_parser()
+
+    timestamps_file = args.timestamps
+    audio_file = args.audio
+    artist = args.artist
+    album = args.album
+    only_cue = args.only_cue
+
+    out_dir = os.path.join(os.getcwd(), f"{artist}_{album}_tracksplit")
+    if not os.path.exists(out_dir):
+        os.mkdir(out_dir)
+
+    timecode, track_string = extract_timestamps(timestamps_file)
+
+    cuesheet_filename = make_cue(
+        timecode, track_string, audio_file, artist, album, out_dir
+    )
+
+    if not only_cue:
+        call_ffcuesplitter(audio_file, cuesheet_filename, out_dir)
+
+
+if __name__ == "__main__":
+    main()
